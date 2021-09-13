b537787d827e6e88c8813e07542c0c7b249a13fe
juanj01707 <90587297+juanj01707@users.noreply.github.com>
2021-09-13 10:53:09 -0500

    modificacion

---
+++ src/poo/parcial/app/AppParc.java
@@ -0,0 +1,7 @@
+package poo.parcial.app;
+
+public class AppParc {
+    public static void main(String[] args) {
+
+    }
+}
--- src/poo/tv/dominio/Televisor.java
+++ src/poo/tv/dominio/Televisor.java
@@ -47,4 +47,5 @@ public class Televisor {
             this.volumenActual--;
         }
     }
+
 }
